1c8f77b5814d41a9039b4e1840e351f63f60fe79
Matthew-Atsidefe <atsidefe@gmail.com>
2022-09-08 15:56:16 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+'#!/bin/bash' >
+4-puts.c
+#include <stdio.h>
+
+/**
+ *  *  main -entry point
+ *   *
+ *    *  Return: 0 after printing the function
+ *     */
+
+int main(void)
+{
+	        puts("\"Programming is like building a multilingual puzzle");
+		        return (0);
+}
