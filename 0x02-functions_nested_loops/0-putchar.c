41603a75e2b15933723ed7e739dd2916df92b5ad
Matthew-Atsidefe <atsidefe@gmail.com>
2022-09-14 03:15:56 -0700

    taks 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+
+/**
+ * main - prints putchar, followed b a new line
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+	_putchar('\n');
+
+	return (0);
+}
